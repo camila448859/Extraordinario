5da1101f34b03b7cc1f4b11ee4666f24ae53f8b7
Mila050802 <anacamilagarciamartines411@gmail.com>
2022-12-12 13:26:19 -0600

    socket

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import statusMonitor from 'express-status-monitor';
+const express = require('express');
+const io = require('socket.io')();
+const statusMonitor = require('express-status-monitor');
 
 const app = express();
-
+// usa el status-monitor middleware para monitorear el status de tu app
 app.use(statusMonitor());
 
+//coneccion
+io.on('connection', (socket) => {
+    setInterval(() => {
+      socket.emit('server-metrics', {
+        memory: process.memoryUsage(),
+        cpu: os.loadavg()
+      });
+    }, 1000);
+  });
+
+//conexion al servidor
+app.listen(3000, () => console.log('server started'));
+
+//redirecccion al inicio
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + 'public');
-});
-
-app.listen(3000, () => console.log('server started'));
+    res.sendFile(__dirname + 'public');
+})
