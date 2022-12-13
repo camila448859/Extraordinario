4d2c6d02b8c04f114ead63d7d12f9cd13c5c3246
Mila050802 <anacamilagarciamartines411@gmail.com>
2022-12-12 21:25:08 -0600

    sockett

--- public/js/cliente.js
+++ public/js/cliente.js
@@ -1,6 +1,6 @@
 const socket = io();
 
 socket.on('server-metrics', (metrics) => {
-  document.getElementById('memory').innerHTML = metrics.memory;
-  document.getElementById('cpu').innerHTML = metrics.cpu;
+  document.getElementById('memory').innerText = metrics.memory;
+  document.getElementById('cpu').innerText = metrics.cpu;
 });
